4a7bc4ae970477786e38d5ffe626ac2acdb1ea2d
Dênis Augusto Luft <denisluft8@gmail.com>
2023-03-13 16:21:02 -0300

    font correction

--- src/Styles/globalStyles.ts
+++ src/Styles/globalStyles.ts
@@ -2,8 +2,8 @@ import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
 @font-face {
-  font-family: 'Hind Siliguri', sans-serif;
-  src: url('https://fonts.googleapis.com/css2?family=Hind+Siliguri:wght@400;500;600;700&display=swap');
+  font-family: 'Cantarell', sans-serif;
+  src: url('https://fonts.googleapis.com/css2?family=Cantarell:wght@400;700&display=swap');
 }
 *{
 margin: 0;
@@ -12,13 +12,15 @@ margin: 0;
 html{
   min-height: 100%;
   scroll-behavior: smooth;
+
 }
 #root{
     height: 100vh;
 }
 body {
   background-color: #eee;
-  font-family: 'Hind Siliguri', sans-serif;
+  font-family: 'Cantarell', sans-serif;
+  line-height: 20px;
   min-height: 100%;
   width: 100%;
 
